daa962890171436a27b655e8f1a559c5a309688d
godchurch <37278203+godchurch@users.noreply.github.com>
2019-09-08 02:53:16 -1000

    split up files

--- install.sh
+++ install.sh
@@ -1,31 +1,15 @@
 #!/bin/sh
 
-# has user specified DEVICE and CHROOT directory
+# check for DEVICE and CHROOT operands
 if test "$#" -ne 2; then
-  printf "Usage: %s [DEVICE] [CHROOT]\n" "$0"; exit 1
+  printf "Usage: %s [DEVICE] [CHROOT]\n" "$0"
+  exit 1
 fi
 
 set -e -x
 
 DEVICE="${1%/}"
 CHROOT="${2%/}"
-
-DESTINATION="$CHROOT/tmp/chroot"
-SKEL="$DESTINATION/skel"
-INSTALL="virtualbox-guest-x11 pulseaudio libavcodec-extra xorg i3 rxvt-unicode firefox"
-PURGE=""
-
-SED="\
-s|DEFAULT_I3_MODIFIER|Mod1|g;
-s|DEFAULT_I3_UP|j|g;
-s|DEFAULT_I3_DOWN|k|g;
-s|DEFAULT_I3_LEFT|h|g;
-s|DEFAULT_I3_RIGHT|l|g;
-s|DEFAULT_BACKGROUND_COLOR|#AF5FAF|g;
-s|DEFAULT_THEME|Adwaita|g;
-s|DEFAULT_FONT|Sans 10|g;
-s|DEFAULT_WINDOW_MANAGER|i3|g;
-s|DEFAULT_BROWSER|firefox|g;"
 
 # mount file systems
 mount "$DEVICE" "$CHROOT"
@@ -37,23 +21,5 @@ mount shm    "$CHROOT/dev/shm" -t tmpfs    -o mode=1777,nosuid,nodev
 mount run    "$CHROOT/run"     -t tmpfs    -o mode=0755,nosuid,nodev
 mount tmp    "$CHROOT/tmp"     -t tmpfs    -o mode=1777,strictatime,nodev,nosuid
 
-test -d "$DESTINATION" || mkdir -p "$DESTINATION"      # does destination exist, create if not
-cp -R skel "$DESTINATION"                             # copy chroot files
-find "$DESTINATION" -type f -exec sed -i "$SED" {} \;  # edit chroot files
-
-if ! test -f "$CHROOT/run/systemd/resolve/stub-resolv.conf"; then
-  mkdir -p "$CHROOT/run/systemd/resolve"
-  echo "nameserver 1.1.1.1" > "$CHROOT/run/systemd/resolve/stub-resolv.conf"
-fi
-
-chroot "$CHROOT" ln -sf ../run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
-
-chroot "$CHROOT" apt-get update -y
-chroot "$CHROOT" apt-get dist-upgrade -y --allow-downgrades
-test -n "$INSTALL" && chroot "$CHROOT" apt-get install -y --no-install-recommends $INSTALL
-test -n "$PURGE" && chroot "$CHROOT" apt-get purge -y $PURGE
-chroot "$CHROOT" apt-get autoremove --purge -y
-chroot "$CHROOT" apt-get clean -y
-
-chroot "$CHROOT" -m -k "$SKEL" user
-chroot "$CHROOT" passwd user
+cp -R src/* "$CHROOT/tmp"
+chroot "$CHROOT" /tmp/chroot.sh
---
+++ src/chroot.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e -x
+
+if ! test -f /run/systemd/resolve/stub-resolv.conf; then
+  mkdir -p /run/systemd/resolve
+  echo "nameserver 1.1.1.1" > /run/systemd/resolve/stub-resolv.conf
+fi
+
+ln -sf ../run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+
+INSTALL="virtualbox-guest-x11 pulseaudio libavcodec-extra xorg i3 rxvt-unicode firefox"
+PURGE=""
+
+apt-get update -y
+apt-get dist-upgrade -y --allow-downgrades
+test -n "$INSTALL" && apt-get install -y --no-install-recommends $INSTALL
+test -n "$PURGE" && apt-get purge -y $PURGE
+apt-get autoremove --purge -y
+apt-get clean -y
+
+test -d /usr/local/bin || mkdir -p /usr/local/bin
+cp -R /tmp/bin/* /usr/local/bin
+
+useradd -m -k /tmp/skel user
+passwd user
