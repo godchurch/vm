010d3d6e694c64016213725dcb5437b73691773b
godchurch <37278203+godchurch@users.noreply.github.com>
2020-05-27 23:09:37 -1000

    clean up script

--- install.sh
+++ install.sh
@@ -2,40 +2,32 @@
 
 set -e
 
+show() { printf "%s\n" "$@"; }
+
 GUEST_ADDITIONS=""
 test "$#" -eq 1 && GUEST_ADDITIONS="$1"
 
 NEW_USER="user"
 
-printf "%s\n" "Checking for root privileges..."
+show "Checking for root privileges..."
 EFFECTIVE_USER_ID="$(id -u)"
 test "$EFFECTIVE_USER_ID" -eq 0
 
-printf "%s\n" "Checking kernel version..."
-CURRENT_KERNEL="$(uname -r 2> /dev/null)"
+show "Updating system..."; apt-get update -y
+show "Upgrading system..."; apt-get dist-upgrade -y
 
-printf "%s\n" "Creating package lists..."
+show "Checking kernel version..."; CURRENT_KERNEL="$(uname -r 2> /dev/null)"
+show "Creating package lists..."
 PACKAGES_TO_INSTALL="build-essential dkms linux-headers-${CURRENT_KERNEL} alsa-utils libavcodec-extra unzip xorg i3 rxvt-unicode firefox-esr mpv"
 PACKAGES_TO_PURGE=""
 
-printf "%s\n" "Updating system..."
-apt-get update --assume-yes
-printf "%s\n" "Upgrading system..."
-apt-get dist-upgrade --assume-yes
-if test -n "$PACKAGES_TO_INSTALL"; then
-  printf "%s\n" "Installing packages..."
-  apt-get install --assume-yes --no-install-recommends $PACKAGES_TO_INSTALL
-fi
-if test -n "$PACKAGES_TO_PURGE"; then
-  printf "%s\n" "Removing packages..."
-  apt-get purge --assume-yes $PACKAGES_TO_PURGE
-fi
-printf "%s\n" "Removing unnecessary packages..."
-apt-get autoremove --purge --assume-yes
-printf "%s\n" "Cleaning local repository..."
-apt-get clean --assume-yes
+test -n "$PACKAGES_TO_INSTALL" && { show "Installing packages..."; apt-get install -y --no-install-recommends $PACKAGES_TO_INSTALL; }
+test -n "$PACKAGES_TO_PURGE" && { show "Removing packages..."; apt-get purge -y $PACKAGES_TO_PURGE; }
 
-printf "%s\n" "Setting up autologin..."
+show "Removing unnecessary packages..."; apt-get autoremove --purge -y
+show "Cleaning local repository..."; apt-get clean -y
+
+show "Setting up autologin..."
 test -d /etc/systemd/system/getty@tty1.service.d || mkdir -p /etc/systemd/system/getty@tty1.service.d
 cat > /etc/systemd/system/getty@tty1.service.d/autologin.conf << _HEREDOC
 [Service]
@@ -43,16 +35,15 @@ ExecStart=
 ExecStart=-/sbin/agetty --autologin $NEW_USER --noclear %I \\\$TERM
 _HEREDOC
 
-printf "%s\n" "Setting up tempfs..."
+show "Setting up tempfs..."
 cat >> /etc/fstab << _HEREDOC
 tmpfs /tmp tmpfs rw,nosuid,nodev 0 0
 tmpfs /ram tmpfs rw 0 0
 _HEREDOC
 
-printf "%s\n" "Copying root files..."
-cp -R root/* /
+show "Copying root files..."; cp -R root/* /
 
-printf "%s\n" "Creating new user..."
+show "Creating new user..."
 useradd -m -k skel -s /usr/local/bin/customlogin "$NEW_USER"
 passwd user << _HEREDOC
 $NEW_USER
@@ -61,4 +52,4 @@ _HEREDOC
 
 test -n "$GUEST_ADDITIONS" && test -x "$GUEST_ADDITIONS" && "$GUEST_ADDITIONS"
 
-printf "%s\n" "Done!"
+show "Done!"
