4c851e7b8353a4d7a6e4309457b00d2d1f80329b
godchurch <37278203+godchurch@users.noreply.github.com>
2020-06-15 17:26:31 -1000

    removed show

--- install.sh
+++ install.sh
@@ -1,54 +1,31 @@
 #!/bin/sh
 
-set -e
-
-show() { printf "%s\n" "$@"; }
-
-GUEST_ADDITIONS=""
-test "$#" -eq 1 && GUEST_ADDITIONS="$1"
-
-NEW_USER="user"
-
-show "Checking for root privileges..."
+set -ex
 EFFECTIVE_USER_ID="$(id -u)"
 test "$EFFECTIVE_USER_ID" -eq 0
-
-show "Updating system..."; apt-get update -y
-show "Upgrading system..."; apt-get dist-upgrade -y
-
-show "Checking kernel version..."; CURRENT_KERNEL="$(uname -r 2> /dev/null)"
-show "Creating package lists..."
-PACKAGES_TO_INSTALL="build-essential dkms linux-headers-${CURRENT_KERNEL} alsa-utils libavcodec-extra unzip xorg i3 xterm firefox-esr mpv"
+NEW_USER="user"
+apt-get update -y
+apt-get dist-upgrade -y
+PACKAGES_TO_INSTALL="alsa-utils pulseaudio libavcodec-extra unzip curl xorg i3 xterm mpv firefox"
 PACKAGES_TO_PURGE=""
-
-test -n "$PACKAGES_TO_INSTALL" && { show "Installing packages..."; apt-get install -y --no-install-recommends $PACKAGES_TO_INSTALL; }
-test -n "$PACKAGES_TO_PURGE" && { show "Removing packages..."; apt-get purge -y $PACKAGES_TO_PURGE; }
-
-show "Removing unnecessary packages..."; apt-get autoremove --purge -y
-show "Cleaning local repository..."; apt-get clean -y
-
-show "Setting up autologin..."
-test -d /etc/systemd/system/getty@tty1.service.d || mkdir -p /etc/systemd/system/getty@tty1.service.d
-cat > /etc/systemd/system/getty@tty1.service.d/autologin.conf << _HEREDOC
+test -n "$PACKAGES_TO_INSTALL" && apt-get install -y --no-install-recommends $PACKAGES_TO_INSTALL
+test -n "$PACKAGES_TO_PURGE" && apt-get purge -y $PACKAGES_TO_PURGE
+apt-get autoremove --purge -y
+apt-get clean -y
+SYSTEMD_SERVICE="/etc/systemd/system/getty@tty1.service.d/autologin.conf"
+test -d "${SYSTEMD_SERVICE%/*}" || mkdir -p "${SYSTEMD_SERVICE%/*}"
+cat > "$SYSTEMD_SERVICE" << _HEREDOC
 [Service]
 ExecStart=
 ExecStart=-/sbin/agetty --autologin $NEW_USER --noclear %I \\\$TERM
 _HEREDOC
-
-show "Creating ram directory..."; mkdir /ram
-show "Setting up tempfs..."
+mkdir -p /ram
 cat >> /etc/fstab << _HEREDOC
-tmpfs /tmp tmpfs rw,nosuid,nodev 0 0
 tmpfs /ram tmpfs rw 0 0
 _HEREDOC
-
-show "Copying root files..."; cp -R "${0%/*}/root"/* /
-
-show "Creating new user..."
+cp -R "${0%/*}/root"/* /
 useradd -m -k "${0%/*}/skel" -s /usr/local/bin/customlogin "$NEW_USER"
 passwd user << _HEREDOC
 $NEW_USER
 $NEW_USER
 _HEREDOC
-
-test -n "$GUEST_ADDITIONS" && test -x "$GUEST_ADDITIONS" && "$GUEST_ADDITIONS"
