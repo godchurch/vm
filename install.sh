fe07c0712da32b84da55e3748b6c0eeea8760a89
godchurch <37278203+godchurch@users.noreply.github.com>
2020-08-13 21:31:25 -1000

    add grub edit

--- install.sh
+++ install.sh
@@ -86,60 +86,34 @@ _EOF
 
 mkdir -p "$BUILD_DIR/etc/apt"
 cat > "$BUILD_DIR/etc/apt/sources.list" << _EOF
-# See http://help.ubuntu.com/community/UpgradeNotes for how to upgrade to
-# newer versions of the distribution.
-deb http://archive.ubuntu.com/ubuntu/ ${CODENAME} main restricted
-# deb-src http://archive.ubuntu.com/ubuntu/ ${CODENAME} main restricted
+deb http://archive.ubuntu.com/ubuntu/ ${CODENAME} main restricted universe multiverse
+# deb-src http://archive.ubuntu.com/ubuntu/ ${CODENAME} main restricted universe multiverse
 
-## Major bug fix updates produced after the final release of the
-## distribution.
-deb http://archive.ubuntu.com/ubuntu/ ${CODENAME}-updates main restricted
-# deb-src http://archive.ubuntu.com/ubuntu/ ${CODENAME}-updates main restricted
+deb http://archive.ubuntu.com/ubuntu/ ${CODENAME}-updates main restricted universe multiverse
+# deb-src http://archive.ubuntu.com/ubuntu/ ${CODENAME}-updates main restricted universe multiverse
 
-## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
-## team. Also, please note that software in universe WILL NOT receive any
-## review or updates from the Ubuntu security team.
-deb http://archive.ubuntu.com/ubuntu/ ${CODENAME} universe
-# deb-src http://archive.ubuntu.com/ubuntu/ ${CODENAME} universe
-deb http://archive.ubuntu.com/ubuntu/ ${CODENAME}-updates universe
-# deb-src http://archive.ubuntu.com/ubuntu/ ${CODENAME}-updates universe
-
-## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
-## team, and may not be under a free licence. Please satisfy yourself as to
-## your rights to use the software. Also, please note that software in
-## multiverse WILL NOT receive any review or updates from the Ubuntu
-## security team.
-deb http://archive.ubuntu.com/ubuntu/ ${CODENAME} multiverse
-# deb-src http://archive.ubuntu.com/ubuntu/ ${CODENAME} multiverse
-deb http://archive.ubuntu.com/ubuntu/ ${CODENAME}-updates multiverse
-# deb-src http://archive.ubuntu.com/ubuntu/ ${CODENAME}-updates multiverse
-
-## N.B. software from this repository may not have been tested as
-## extensively as that contained in the main release, although it includes
-## newer versions of some applications which may provide useful features.
-## Also, please note that software in backports WILL NOT receive any review
-## or updates from the Ubuntu security team.
 deb http://archive.ubuntu.com/ubuntu/ ${CODENAME}-backports main restricted universe multiverse
 # deb-src http://archive.ubuntu.com/ubuntu/ ${CODENAME}-backports main restricted universe multiverse
 
-## Uncomment the following two lines to add software from Canonical's
-## 'partner' repository.
-## This software is not part of Ubuntu, but is offered by Canonical and the
-## respective vendors as a service to Ubuntu users.
 # deb http://archive.canonical.com/ubuntu ${CODENAME} partner
 # deb-src http://archive.canonical.com/ubuntu ${CODENAME} partner
 
-deb http://security.ubuntu.com/ubuntu ${CODENAME}-security main restricted
-# deb-src http://security.ubuntu.com/ubuntu ${CODENAME}-security main restricted
-deb http://security.ubuntu.com/ubuntu ${CODENAME}-security universe
-# deb-src http://security.ubuntu.com/ubuntu ${CODENAME}-security universe
-deb http://security.ubuntu.com/ubuntu ${CODENAME}-security multiverse
-# deb-src http://security.ubuntu.com/ubuntu ${CODENAME}-security multiverse
+deb http://security.ubuntu.com/ubuntu ${CODENAME}-security main restricted universe multiverse
+# deb-src http://security.ubuntu.com/ubuntu ${CODENAME}-security main restricted universe multiverse
 _EOF
 
 LC_ALL=C chroot "$BUILD_DIR" /bin/sh -c "#!/bin/sh
 set -e
 set -x
+apt-get update -y
+apt-get dist-upgrade -y
+test -n '$PKGS_TO_INSTALL' && apt-get install -y $PKGS_TO_INSTALL
+test -n '$PKGS_TO_PURGE' && apt-get purge -y $PKGS_TO_PURGE
+apt-get autoremove --purge -y
+apt-get clean -y
+sed 's/^\(GRUB_CMDLINE_LINUX_DEFAULT\)=\".*\"$/#&\\
+\1=\"\"' /etc/default/grub > /tmp/grub_file
+cp /tmp/grub_file /etc/default/grub
 mkdir -p '$SYSTEMD_SERVICE_DIR'
 cat > '$AUTOLOGIN_SERVICE' << _HEREDOC
 [Service]
@@ -159,10 +133,4 @@ useradd -m -k /tmp/skel -d '$USERS_HOME' -s '$LOGIN_SCRIPT' '$DEFAULT_USERNAME'
 passwd '$DEFAULT_USERNAME' << _HEREDOC
 $DEFAULT_USERNAME
 $DEFAULT_USERNAME
-_HEREDOC
-apt-get update -y
-apt-get dist-upgrade -y
-test -n '$PKGS_TO_INSTALL' && apt-get install -y $PKGS_TO_INSTALL
-test -n '$PKGS_TO_PURGE' && apt-get purge -y $PKGS_TO_PURGE
-apt-get autoremove --purge -y
-apt-get clean -y"
+_HEREDOC"
